f26232bb9890a6b7d19dcb8b812734d87672fa70
rzodkiew <rzodkiew@example.com>
2024-04-24 20:28:00 +0200

    siur

---
+++ if.js
@@ -0,0 +1,14 @@
+
+ let ptak = 1
+
+
+  switch(ptak){
+
+    case 1: 
+      console.log("ptaszysko pierdolone")
+
+      case 2:
+        console.log("ptaszysko nie jest pierdolony")
+
+  }
+      
